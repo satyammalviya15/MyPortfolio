68f5ecbb6a02043a4e5e19c76f1a7e935dc779f1
Satyam Malviya <satyammalviya281@gmail.com>
2024-12-18 01:18:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["FullStack Developer", "Python Developer", "React Developer","Web Developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
